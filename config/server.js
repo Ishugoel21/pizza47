92f812e8a19d090d960d003a93c84d339d974c1a
ishugoel21 <goelishu868@gmail.com>
2024-12-29 22:05:51 +0530

    first

---
+++ config/server.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'), // Ensures Strapi listens on all available interfaces
+  port: env.int('PORT', process.env.PORT || 1337), // Dynamically sets the port
+  url: env('PUBLIC_URL', 'http://localhost:1337/'), // Your public URL
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  proxy: true, // Optional: usually used for reverse proxies like Azure
+});
