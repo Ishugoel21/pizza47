3544524a3dcdcb661a7b1964a836d1e9d47519db
Ishu goel <ishu.22scse1180016@galgotiasuniversity.edu.in>
2024-12-30 12:37:04 +0530

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,14 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
   'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://pizza76.web.app', 
+        'https://pizza76.firebaseapp.com'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
 ];
