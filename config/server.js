797ffad627bbabb026a50635c07d44babae2c9ce
Ishu goel <ishu.22scse1180016@galgotiasuniversity.edu.in>
2024-12-30 12:20:42 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'), // Ensure it listens on all available network interfaces
   port: env.int('PORT', process.env.PORT || 1337), // Dynamically use the port provided by Render
-  url: env('PUBLIC_URL', process.env.PUBLIC_URL || 'http://localhost:1337/'), // Ensure PUBLIC_URL uses Render's environment variable if set
+  url: env('PUBLIC_URL', process.env.PUBLIC_URL || 'https://pizza4.onrender.com/'), // Ensure PUBLIC_URL uses Render's environment variable if set
   app: {
     keys: env.array('APP_KEYS'),
   },
