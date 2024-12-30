57fd88d3b627a54a8f3db1275b1c75bac7e76bef
Ishu goel <ishu.22scse1180016@galgotiasuniversity.edu.in>
2024-12-31 00:23:58 +0530

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,18 @@
-module.exports = {
-  upload: {
-    provider: 'cloudinary',
-    providerOptions: {
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,  // Use the environment variable
-      api_key: process.env.CLOUDINARY_API_KEY,        // Use the environment variable
-      api_secret: process.env.CLOUDINARY_API_SECRET,  // Use the environment variable
-      folder: 'strapi-media',                         // Optional: Specify a folder name for organization
-    },
-  },
-};
+    module.exports = ({ env }) => ({
+      // ...
+      upload: {
+        config: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+          },
+          actionOptions: {
+            upload: {},
+            delete: {},
+          },
+        },
+      },
+      // ...
+    });
