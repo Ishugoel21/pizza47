cf4f76f2d1fbdad538615e793007e364d218ed17
Ishu goel <ishu.22scse1180016@galgotiasuniversity.edu.in>
2025-02-01 10:44:01 +0530

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-module.exports = {
-  upload: {
-    provider: 'local', // Use the local file system for image storage
-    providerOptions: {
-      sizeLimit: 50000000, // Set the file size limit to 50 MB (50 * 1024 * 1024)
-    },
-  },
-};
-=======
-require('dotenv').config();
-const cloudinary = require('cloudinary').v2; // Assuming you're using Cloudinary
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
-});
 
 module.exports = ({ env }) => ({
   upload: {
@@ -33,4 +16,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
->>>>>>> d009c98fb05893330afc10f098184c733673e1b5
